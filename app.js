83d7859c3ed6c21262e1bb8e8f47e582d1fd2e79
KcPele <fidekg123@gmail.com>
2022-06-13 14:29:35 +0100

    added time

--- app.js
+++ app.js
@@ -4,7 +4,10 @@ const app = express()
 const port = process.env.PORT || 3000
 
 
-
+setInterval(function A() {
+    console.log("started")
+    bot()
+}, 2.16e+7);
 app.get("/", async(req, res) => {
     bot()
     return res.json({"msg": "bot runing"})
